caa30792af14ebf1a504eb77a2a5e674b958e9eb
sneko <supernekocat31@gmail.com>
2025-01-18 17:56:20 +0900

    add index route

---
+++ app/routes/home.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <p id="index-page">
+      これはReact Routerのデモです。
+      <br />
+      reactrouter.comでドキュメントをご覧ください。
+      <a href="https://reactrouter.com">
+        reactrouter.com
+      </a>
+      。
+    </p>
+  );
+}
