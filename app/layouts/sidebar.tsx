88ff7dfe947090d50de21a807d981d353c3fd0d7
sneko <supernekocat31@gmail.com>
2025-01-25 11:36:46 +0900

    fix:fix History Stack Management

--- app/layouts/sidebar.tsx
+++ app/layouts/sidebar.tsx
@@ -32,7 +32,12 @@ export default function SidebarLayout({ loaderData, }: Route.ComponentProps) {
         <div>
           <Form
             id="search-form"
-            onChange={(event) => submit(event.currentTarget)}
+            onChange={(event) => {
+              const isFirstSearch = q === null;
+              submit(event.currentTarget, {
+                replace: !isFirstSearch,
+              });
+            }}
             role="search"
           >
             <input
